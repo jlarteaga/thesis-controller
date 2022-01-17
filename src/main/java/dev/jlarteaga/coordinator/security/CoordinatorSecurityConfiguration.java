f805ca82af441a08533150ada2ad3901561a9efa
Jorge Luis Arteaga Almaraz <jorgitoaral@gmail.com>
2022-01-17 01:23:40 -0400

    chore: Added CORS configuration.

--- src/main/java/dev/jlarteaga/coordinator/security/CoordinatorSecurityConfiguration.java
+++ src/main/java/dev/jlarteaga/coordinator/security/CoordinatorSecurityConfiguration.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @EnableWebFluxSecurity
 public class CoordinatorSecurityConfiguration {
@@ -23,7 +26,14 @@ public class CoordinatorSecurityConfiguration {
         return http.csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
-                .cors().and()
+                .cors().configurationSource(exchange -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(List.of("*"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.setAllowedMethods(List.of("OPTIONS", "POST"));
+                    corsConfiguration.setMaxAge(600L);
+                    return corsConfiguration;
+                }).and()
                 .exceptionHandling()
                 .authenticationEntryPoint((swe, e) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)))
                 .accessDeniedHandler((swe, e) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)))
